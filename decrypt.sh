56983a9de3a2208eb7065adf6aa0368325f0e2c0
Archer Heffern <heffernarcher@gmail.com>
2025-04-20 05:36:25 +0000

    fzf for encrypted files and better documentation

--- decrypt.sh
+++ decrypt.sh
@@ -2,17 +2,29 @@
 
 set -euo pipefail
 
+HELP_MESSAGE=$(cat << 'EOF'
+    Usage: decrypt FILE: Decrypt FILE
+    Usage: decrypt -h|-help|--help: Display this help message
+    Usage: decrypt -s|-select|--select: Select file to decrypt using fzf. Displays only the base filenames
+    All files must be of the form <algorithm>:<filename>.enc
+EOF
+)
+
 if [[ -z ${1:-} ]]; then
-	echo "Usage: decrypt FILE" >&2
+	echo "$HELP_MESSAGE" >&2
 	exit 1
 fi
 file="$1"
 
 if [[ "$file" = "-h" || "$file" = "-help" || "$file" = "--help" ]]; then
-	echo "Usage: decrypt FILE"
-	echo "All files must be of the form <algorithm>:<filename>.enc"
+	echo "$HELP_MESSAGE"
 	exit 0
 fi
+
+if [[ "$file" = "-s" || "$file" = "-select" || "$file" = "--select" ]]; then
+	file=$(fdfind -d1 -Ie enc . | fzf)
+	echo "Selected ${file}"
+fi 
 
 if [[ ! -e "$file" ]]; then
 	echo "FILE does not exist"
